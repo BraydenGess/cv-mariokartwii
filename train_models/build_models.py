40efe98aad8741a49678955921388cbd511d731d
bradygess <bradygess13@gmail.com>
2023-12-30 20:22:17 -0500

    update

--- train_models/build_models.py
+++ train_models/build_models.py
@@ -1,1 +1,17 @@
 import cv2 as cv
+import sys
+
+def set_homemodelparameters():
+    coordinates = [135,490,80,180]
+    training_folder = 'training_images/home_trainingimages/'
+    label_key = {'None':'0','Home':'1'}
+    img = cv.imread('training_images/home_trainingimages/Home#0.png')
+    newframe = img[80:180,135:490]
+    cv.imwrite('cack.png',newframe)
+
+def main():
+    set_homemodelparameters()
+    print(sys.argv)
+
+
+main()
