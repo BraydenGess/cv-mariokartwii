58836835ac25f3133739fb73b29bc51818b19204
bradygess <bradygess13@gmail.com>
2023-12-30 20:01:00 -0500

    update

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+
+setup(name='cv-mariokartwii',
+      version = '1.0',
+      description = 'Computer Vision MarioKart Wii Music and Stats',
+      packages=find_packages(),
+      py_modules=['training_images'])
