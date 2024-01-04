bcee8c8fbfa56d323ec54e31ae23cba502bb39fb
bradygess <bradygess13@gmail.com>
2024-01-04 16:03:32 -0500

    update

--- character_selection.py
+++ character_selection.py
@@ -2,6 +2,7 @@ from tools.deep_learning import predict
 
 def menu_control(frame,coordinates,root_model):
     index,confidence = predict(frame,coordinates.menu_coordinates,root_model.menudetect_model,'sharpimgtobinary')
+    print(index)
     return 42
 
 
