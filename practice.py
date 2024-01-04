a753b8c8f55e51b85d117b89359eb36e8c65c3d1
bradygess <bradygess13@gmail.com>
2024-01-04 18:41:07 -0500

    update

--- practice.py
+++ practice.py
@@ -5,12 +5,11 @@ import cv2 as cv
 from tools.imagemanipulation import imgtobinary
 import numpy as np
 import os
-img_path = 'train_models/training_images/menu_trainingimages/Start#A.png'
-dup_path = 'train_models/training_images/menu_trainingimages/Start#C.png'
-img = cv.imread(img_path)
-cv.imwrite(dup_path,img)
 
-
+cap = cv.VideoCapture(0)
+ret,frame = cap.read()
+print(frame)
+cv.imwrite('train_models/training_images/home_trainingimages/Home#14.png', imgtobinary(frame))
 
 
 
