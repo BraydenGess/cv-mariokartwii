29795a3e734fc8c0af6981698d092ae1fcd637b1
bradygess <bradygess13@gmail.com>
2024-01-04 20:16:00 -0500

    update

--- practice.py
+++ practice.py
@@ -5,8 +5,8 @@ import cv2 as cv
 from tools.imagemanipulation import imgtobinary
 import numpy as np
 import os
-path = 'train_models/training_images/menu_trainingimages/Multiplayer#K.png'
-new_path = 'train_models/training_images/playercount_trainingimages/players4#B.png'
+path = 'train_models/training_images/menu_trainingimages/Start#B.png'
+new_path = 'train_models/training_images/playercount_trainingimages/None#F.png'
 cv.imwrite(new_path,cv.imread(path))
 
 
