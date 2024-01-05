6ebb41e084978bd3b9203d91332652806d3eb05a
bradygess <bradygess13@gmail.com>
2024-01-04 19:14:45 -0500

    update

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ import cv2 as cv
 import time
 from character_selection import character_select
 
+def safety_check(sp):
+    warning = False
+    safe = False
+    while not safe:
+        if sp.spotify.current_playback() != None:
+            safe = True
+        elif not warning:
+            print('Activate Device')
+            warning = True
+    print('Ready')
+
 def main():
     sp, coordinates = audio_setup(genre='rock', credentials_file='credentials.txt')
     root_model = initialize_rootmodel()
     cap = cv.VideoCapture(0)
-    print('Ready')
+    safety_check(sp)
     while cap.isOpened():
         ret,frame = cap.read()
         run_audio(sp,frame,root_model,coordinates)
