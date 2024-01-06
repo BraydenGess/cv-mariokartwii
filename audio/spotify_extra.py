8bfa2c21ff386e4b1ec470cbf7cceb1c5d4e3717
bradygess <bradygess13@gmail.com>
2024-01-06 18:16:59 -0500

    update

--- audio/spotify_extra.py
+++ audio/spotify_extra.py
@@ -1,15 +1,39 @@
 from spotify_audio import setup_spotifyobject
 import sys
 
-def main():
-    command = sys.argv
-    sp = setup_spotifyobject(file='credentials.txt')
-    username = sp.current_user()['id']
-    playlists = sp.user_playlists(username)
+def check_playlistexists(playlists,target_playlist):
     for i, playlist in enumerate(playlists['items']):
-        print(playlist['name'])
-        #playlist_name = playlist['items']['name']
-        #print(playlist_name)
+        if target_playlist == playlist['name']:
+            return True,playlist
+    print('Playlist Not Found')
+    print("Hint: Make sure playlist is public and added to username's profile")
+    sys.exit()
+    return False,None
+def make_newplaylist(sp,username,target_playlist):
+    playlists = sp.user_playlists(username)
+    found,playlist = check_playlistexists(playlists,target_playlist)
+    if found:
+        tracks = sp.playlist_tracks(playlist_id=playlist['uri'])
+        for track in tracks['items']:
+            name = track['track']['name']
+            uri = track['track']['uri']
+            print(name,uri)
+def get_arguements(argv,sp):
+    command = argv[1]
+    if command == 'newplaylist':
+        target_playlist = argv[2]
+        username = sp.current_user()['id']
+        if len(command) >= 4:
+            username = argv[3]
+    return command,username,target_playlist
+
+def main():
+    sp = setup_spotifyobject(file='credentials.txt')
+    command,username,target_playlist = get_arguements(sys.argv,sp)
+    print(command,username,target_playlist)
+    if command == 'newplaylist':
+        make_newplaylist(sp,username,target_playlist)
+
 
 
 
