c7069ed3abe6cdc257905b3d1e1f8726666811a4
bradygess <bradygess13@gmail.com>
2024-01-10 14:13:16 -0500

    update

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ from character_selection import character_select
 from graphics.graphics import initialize_graphics
 from nextgenstats.live import nextgenstats
 
-def safe_pull(cap):
-    ret,frame = cap.read()
-    if not ret:
-        print('No Input')
-        sys.exit()
-    return frame
-
 def main():
     sp, coordinates = audio_setup(genre='Rock', credentials_file='credentials.txt')
     root_model,gp_info = initialize_rootmodel(),initialize_gpinfo()
